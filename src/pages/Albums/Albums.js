9afbacd9a9378f55dceadede8d615da57882c730
rubzen <38146181+rubzen@users.noreply.github.com>
2018-09-25 18:05:34 -0500

    Ok

--- src/pages/Albums/Albums.js
+++ src/pages/Albums/Albums.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class Album extends Component {
+export default class Albums extends Component {
 	state = {
-		loading: true,
+		laoding: true,
 		error: null,
 		album: null
 	};
-
-	componentDidMount() {
-		this.loadAlbum();
+	constructor() {
+		super();
+		console.log('1 Album constructor');
 	}
 
-	loadAlbum = () => {
+	componentDidMount() {
+		this.loadAlbums();
+		console.log('3. Album did mount');
+	}
+
+	componentDidUpdate(prevprops) {
+		console.log('4 Album did update');
+		if (this.props.match.params.albumsId !== prevprops.match.params.albumsId) {
+			this.loadAlbums();
+		}
+	}
+
+	componentWillUnmount() {
+		console.log('5 Album will unmount');
+	}
+
+	loadAlbums = () => {
+		console.log('Loading Albums');
 		this.setState({
 			loading: true,
 			error: null
 		});
-		/* https://react-api-lab.herokuapp.com/albums/83d91898-7763-47d7-b03b-b92132375c47 */
-		fetch(`https://react-api-lab.herokuapp.com/albums/${this.props.match.params.albumId}`)
+
+		fetch(`https://react-api-lab.herokuapp.com/albums/${this.props.match.params.albumsId}`)
 			.then((response) => response.json())
 			.then((data) => {
 				this.setState({
@@ -34,17 +51,18 @@ class Album extends Component {
 			});
 	};
 	render() {
+		console.log('2 Album render');
 		const { loading, album, error } = this.state;
 		return (
-			<div className="albumInfo">
-				{loading && 'loading music...'}
+			<div className="albuminfo">
+				{loading && 'loading albums...'}
 				{album && (
 					<React.Fragment>
 						<img src={album.imageUrl} />
 						<p>{album.name}</p>
-						<p>Tracks:</p>
+						<p>Tracks: </p>
 						{album.tracks.map((track) => {
-							return <div>{track.name}</div>;
+							return <div>{track.names}</div>;
 						})}
 					</React.Fragment>
 				)}
@@ -52,5 +70,3 @@ class Album extends Component {
 		);
 	}
 }
-
-export default Album;
